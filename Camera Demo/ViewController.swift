26f855b1090059850db515286236dbb7a05b60cc
Soumil <LPTP233@Joys-MacBook-Pro.local>
2019-04-18 14:32:57 +0530

    Update

--- Camera Demo/ViewController.swift
+++ Camera Demo/ViewController.swift
@@ -11,11 +11,18 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageViewer: UIImageView!
+    @IBOutlet weak var saveButton: UIButton!
+    var newImage: UIImage?
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    /* Description: Opening Camera Action
+     - Parameter keys: sender
+     - Returns: No Parameter
+     */
     @IBAction func takeImageAction(_ sender: UIButton) {
+        saveButton.isEnabled = false
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
             let imagePicker = UIImagePickerController()
             imagePicker.delegate = self
@@ -30,26 +37,44 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
     
+    /* Description: Show the picked image in Imageview
+     - Parameter keys: picker, info
+     - Returns: No Parameter
+     */
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let pickedImage = info[.originalImage] as? UIImage {
             imageViewer.contentMode = .scaleToFill
             imageViewer.image = pickedImage
-            UIImageWriteToSavedPhotosAlbum(pickedImage, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)            
-            dismiss(animated: true, completion: nil)
+            newImage = pickedImage
+            saveButton.isEnabled = true
         }
         picker.dismiss(animated: true, completion: nil)
     }
     
+    /* Description: Save Image button action
+     - Parameter keys: sender
+     - Returns: No Parameter
+     */
+    @IBAction func saveImageAction(_ sender: UIButton) {
+        UIImageWriteToSavedPhotosAlbum(newImage!, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /* Description: Save Image to Photos
+     - Parameter keys: image, error, contextInfo
+     - Returns: No Parameter
+     */
     @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
         if let error = error {
             // we got back an error!
-            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .default))
-            present(ac, animated: true)
+            let errorAlert = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            errorAlert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(errorAlert, animated: true)
         } else {
-            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .default))
-            present(ac, animated: true)
+            let successAlert = UIAlertController(title: "Saved!", message: "Your image has been saved to your photos.", preferredStyle: .alert)
+            successAlert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(successAlert, animated: true)
+            saveButton.isEnabled = false
         }
     }
     
